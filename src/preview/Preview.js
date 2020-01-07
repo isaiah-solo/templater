dbd7cbc5b2849f381d9036b064e62d12b484ebef
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2020-01-07 02:30:37 -0800

    added divs to preview to show basic texts

--- src/preview/Preview.js
+++ src/preview/Preview.js
@@ -1,13 +1,40 @@
 // @flow strict
 
 import type {Element} from 'react';
-import React from 'react';
+
+import type {Item, State} from '../reducer/workspaceItemReducer';
+
+import React, {useMemo} from 'react';
+
+import {useSelector} from "react-redux";
+
+export const PADDING = 20;
 
 type Props = $ReadOnly<{||}>;
 
 function Preview(_: Props): Element<'div'> {
+  const items = useSelector((state?: State): Array<Item> => {
+    return state !== undefined ? state.items : [];
+  });
+  const itemElements = useMemo(
+    (): Array<Element<'div'>> => {
+      return items.map((
+        {text}: Item,
+        index: number,
+      ): Element<'div'> => {
+        return (
+          <div key={index}>
+            {text}
+          </div>
+        );
+      })
+    },
+    [items],
+  );
   return (
-    <div style={styles.root} />
+    <div style={styles.root}>
+      {itemElements}
+    </div>
   );
 }
 
@@ -15,7 +42,9 @@ const styles = {
   root: {
     backgroundColor: 'white',
     boxShadow: 'rgba(0, 0, 0, 0.05) 0 2px 16px -2px',
+    boxSizing: 'border-box',
     height: '100%',
+    padding: PADDING,
     width: '100%',
   },
 };
